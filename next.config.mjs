49600706d376599e1f672ec36631f44335a418e6
Yogesh Singh <ayogesh1214@gmail.com>
2025-08-07 19:37:04 +0530

    ignore build errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true, // 🚨 disables TypeScript errors during build
+  },
+};
 
 export default nextConfig;
